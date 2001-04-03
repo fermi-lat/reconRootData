a20b96aa65f2045d4e0c1a0cc7f173315292fa4e
cvs2git <>
2001-04-03 17:26:39 +0000

    This commit was manufactured by cvs2svn to create tag 'v1'.
    Sprout from master 2001-04-03 17:26:38 UTC heather 'update documentation'
    Cherrypick from master 2001-03-31 01:19:40 UTC tlindner 'AutoUpdate':
        ChangeLog
    Delete:
        cmt/setup.bat
        cmt/setup.csh
        cmt/setup.sh

--- cmt/setup.sh
+++
@@ -1,7 +0,0 @@
-# echo "Setting reconRoot v0 in I:/packages/Root2IDL"
-
-CMTROOT=I:\packages/CMT/v1r5p1; export CMTROOT
-. ${CMTROOT}/mgr/setup.sh
-
-tempfile=/tmp/setup$$
-${CMTROOT}/mgr/cmt -quiet setup -sh -pack=reconRoot -version=v0 -path=I:/packages/Root2IDL $* >$tempfile; . $tempfile; /bin/rm -f $tempfile
