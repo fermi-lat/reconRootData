3dfa1bea4a34d326e03c16b5042f58635eb3bcfe
heather <>
2001-10-26 19:02:19 +0000

    Fix memory leaks and use automatic streamers

--- reconRootData/ReconLinkDef.h
+++ reconRootData/ReconLinkDef.h
@@ -4,18 +4,18 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class TkrSiCluster;
-#pragma link C++ class TkrHit;
-#pragma link C++ class TkrRecon;
-#pragma link C++ class TkrTrack;
-#pragma link C++ class TkrLocator;
-#pragma link C++ class TkrGamma;
+#pragma link C++ class TkrSiCluster+;
+#pragma link C++ class TkrHit+;
+#pragma link C++ class TkrRecon+;
+#pragma link C++ class TkrTrack+;
+#pragma link C++ class TkrLocator+;
+#pragma link C++ class TkrGamma+;
 
-#pragma link C++ class CalRecon;
-#pragma link C++ class CalLogEne;
-#pragma link C++ class CalCluster;
+#pragma link C++ class CalRecon+;
+#pragma link C++ class CalLogEne+;
+#pragma link C++ class CalCluster+;
 
-#pragma link C++ class Recon;
-#pragma link C++ class ReconHeader;
+#pragma link C++ class Recon+;
+#pragma link C++ class ReconHeader+;
 
 #endif
