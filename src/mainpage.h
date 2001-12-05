e78ee84385d70350f6dd2f14492dd52f0cb3d3bc
heather <>
2001-12-05 17:03:51 +0000

    move mainpage.h to src directory

---
+++ src/mainpage.h
@@ -0,0 +1,16 @@
+// Mainpage for doxygen
+
+/*! \mainpage package reconRootData
+
+  \section intro Introduction
+
+  This is the set of ROOT classes used to store reconstruction data.
+
+  <hr>
+  \section notes release notes
+  \include release.notes
+  \section requirements requirements
+  \include requirements
+
+*/
+
