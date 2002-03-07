aea328bd24c475dee6664198b763328c61e4052d
cvs2git <>
2002-03-07 22:10:06 +0000

    This commit was manufactured by cvs2svn to create tag 'v2r1p3'.
    Sprout from cmt-patches-v1r10p20011126 2002-03-07 22:07:39 UTC heather 'automate rootcint generation on windows'
    Delete:
        reconRootData/ReconCint.cxx
        reconRootData/ReconCint.h
        reconRootData/dorootcint.bat

--- reconRootData/dorootcint.bat
+++
@@ -1,3 +0,0 @@
-REM do rootcint for Recon classes
-cd ..\reconRootData
-%ROOTSYS%\bin\rootcint -f ReconCint.cxx -c -I%DIGIROOTDATAROOT% CalCluster.h CalLogEne.h CalRecon.h Recon.h ReconHeader.h TkrGamma.h TkrHit.h TkrLocator.h TkrRecon.h TkrSiCluster.h TkrTrack.h ReconLinkDef.h
