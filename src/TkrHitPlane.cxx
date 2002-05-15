a1b06a5f6ec436ee31eded6a9bb3a1fc2e248e3b
usher <>
2002-05-15 20:45:16 +0000

    Modifications for new tracker root output classes: 1) Removed the unused TkrGamma, TkrHit and TkrLocator files 2) Add the new files for pat rec, fit track and vertex output

---
+++ src/TkrHitPlane.cxx
@@ -0,0 +1,41 @@
+#include "reconRootData/TkrHitPlane.h"
+
+ClassImp(TkrHitPlane)
+
+// Default constructor
+TkrHitPlane::TkrHitPlane() 
+{
+    // Make sure everything is properly zeroed
+    m_IdHit      = 0;
+    m_IdTower    = 0.;
+    m_IdPlane    = 0.;
+    m_Projection = AXIS::NONE;
+    m_ProjPlus   = AXIS::NONE;
+    m_Zplane     = 0.;
+    m_EnePlane   = 0.;
+    m_RadLen     = 0.;
+}
+
+void TkrHitPlane::initializeInfo(UInt_t hit, UInt_t tower, UInt_t plane, AXIS proj,
+                                 AXIS nextProj, Double_t z, Double_t energy, Double_t radLen)
+{
+    m_IdHit      = hit;
+    m_IdTower    = tower;
+    m_IdPlane    = plane;
+    m_Projection = proj;
+    m_ProjPlus   = nextProj;
+    m_Zplane     = z;
+    m_EnePlane   = energy;
+    m_RadLen     = radLen;
+}
+
+void TkrHitPlane::initializeHits(const TkrFitHit& meas, const TkrFitHit& pred, const TkrFitHit& fit,
+                                 const TkrFitHit& smooth, const TkrCovMat& material)
+{
+    m_HitMeas   = meas;
+    m_HitPred   = pred;
+    m_HitFit    = fit;
+    m_HitSmooth = smooth;
+
+    m_Qmaterial = material;
+}
