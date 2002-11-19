1d999eb29054240b42fa9e20f1c8ab8fc881d4e5
heather <>
2002-11-19 17:36:15 +0000

    update to fix handling of TkrParams and TkrCovMat to really mirror what is present in the Event package for the TDS

--- reconRootData/TkrCovMat.h
+++ reconRootData/TkrCovMat.h
@@ -21,8 +21,11 @@ public:
 
     TkrCovMat(const TkrCovMat& covMat);
 
-    TkrCovMat(Double_t sigXpos, Double_t sigXslp, Double_t sigXps, 
-              Double_t sigYpos, Double_t sigYslp, Double_t sigYps); 
+    TkrCovMat( Double_t e_00, Double_t e_01, Double_t e_02, Double_t e_03,
+        Double_t e_10, Double_t e_11, Double_t e_12, Double_t e_13,
+        Double_t e_20, Double_t e_21, Double_t e_22, Double_t e_23,
+        Double_t e_30, Double_t e_31, Double_t e_32, Double_t e_33);
+
 
     virtual ~TkrCovMat() {}
 
@@ -30,19 +33,34 @@ public:
 
     TkrCovMat& operator=(const TkrCovMat& covMat);
 
-    //! Provide an initialization method (in case of default constructor)
-    void     initialize(Double_t sigXpos, Double_t sigXslp, Double_t sigXps, 
-                        Double_t sigYpos, Double_t sigYslp, Double_t sigYps);
     void     initialize(const TkrCovMat& covMat);
 
-    //! Methods to return errors 
-    //! (Are these redundant with the inheritance of TMatrixD?)
-    Double_t getCovX0X0() const {return GetNoElements() == 16 ? operator()(0,0) : -9999.;}
-    Double_t getCovSxSx() const {return GetNoElements() == 16 ? operator()(1,1) : -9999.;}
-    Double_t getCovX0Sx() const {return GetNoElements() == 16 ? operator()(0,1) : -9999.;}
-    Double_t getCovY0Y0() const {return GetNoElements() == 16 ? operator()(2,2) : -9999.;}
-    Double_t getCovSySy() const {return GetNoElements() == 16 ? operator()(3,3) : -9999.;}
-    Double_t getCovY0Sy() const {return GetNoElements() == 16 ? operator()(2,3) : -9999.;}
+    void initialize(Double_t e_00, Double_t e_01, Double_t e_02, Double_t e_03,
+        Double_t e_10, Double_t e_11, Double_t e_12, Double_t e_13,
+        Double_t e_20, Double_t e_21, Double_t e_22, Double_t e_23,
+        Double_t e_30, Double_t e_31, Double_t e_32, Double_t e_33);						 
+
+
+    /// Methods to return errors 
+    inline Double_t getCovX0X0() const {return GetNoElements() == 16 ? operator()(0,0) : -9999.;}
+    inline Double_t getCovX0Sx() const {return GetNoElements() == 16 ? operator()(0,1) : -9999.;}
+    inline Double_t getCovX0Y0() const {return GetNoElements() == 16 ? operator()(0,2) : -9999.;}
+    inline Double_t getCovX0Sy() const {return GetNoElements() == 16 ? operator()(0,3) : -9999.;}
+
+    inline Double_t getCovSxX0() const {return GetNoElements() == 16 ? operator()(1,0) : -9999.;}
+    inline Double_t getCovSxSx() const {return GetNoElements() == 16 ? operator()(1,1) : -9999.;}
+    inline Double_t getCovSxY0() const {return GetNoElements() == 16 ? operator()(1,2) : -9999.;}
+    inline Double_t getCovSxSy() const {return GetNoElements() == 16 ? operator()(1,3) : -9999.;}
+
+    inline Double_t getCovY0X0() const {return GetNoElements() == 16 ? operator()(2,0) : -9999.;}
+    inline Double_t getCovY0Sx() const {return GetNoElements() == 16 ? operator()(2,1) : -9999.;}
+    inline Double_t getCovY0Y0() const {return GetNoElements() == 16 ? operator()(2,2) : -9999.;}
+    inline Double_t getCovY0Sy() const {return GetNoElements() == 16 ? operator()(2,3) : -9999.;}
+
+    inline Double_t getCovSyX0() const {return GetNoElements() == 16 ? operator()(3,0) : -9999.;}
+    inline Double_t getCovSySx() const {return GetNoElements() == 16 ? operator()(3,1) : -9999.;}
+    inline Double_t getCovSyY0() const {return GetNoElements() == 16 ? operator()(3,2) : -9999.;}
+    inline Double_t getCovSySy() const {return GetNoElements() == 16 ? operator()(3,3) : -9999.;}
 
 private:
 
--- src/TkrCovMat.cxx
+++ src/TkrCovMat.cxx
@@ -15,47 +15,74 @@ TkrCovMat& TkrCovMat::operator=(const TkrCovMat& covMat)
     return *this;
 }
 
-TkrCovMat::TkrCovMat(Double_t sigXpos, Double_t sigXslp, Double_t sigXps, 
-                     Double_t sigYpos, Double_t sigYslp, Double_t sigYps) :
-           TMatrixD(4,4)
+TkrCovMat::TkrCovMat(double e_00, double e_01, double e_02, double e_03,
+                     double e_10, double e_11, double e_12, double e_13,
+                     double e_20, double e_21, double e_22, double e_23,
+                     double e_30, double e_31, double e_32, double e_33) 
 {
-    initialize(sigXpos, sigXslp, sigXps, sigYpos, sigYslp, sigYps);
+    initialize( e_00, e_01, e_02, e_03, e_10, e_11, e_12, e_13,
+        e_20, e_21, e_22, e_23, e_30, e_31, e_32, e_33);
 }
 
 
-
-void TkrCovMat::initialize(Double_t sigXpos, Double_t sigXslp, Double_t sigXps, 
-                           Double_t sigYpos, Double_t sigYslp, Double_t sigYps)
+void TkrCovMat::initialize(double e_00, double e_01, double e_02, double e_03,
+                           double e_10, double e_11, double e_12, double e_13,
+                           double e_20, double e_21, double e_22, double e_23,
+                           double e_30, double e_31, double e_32, double e_33) 
 {
     if (GetNrows() != 4 || GetNcols() != 4) ResizeTo(4,4);
 
     Zero();
     
-    operator()(0,0) = sigXpos;
-    operator()(1,1) = sigXslp;
-    operator()(0,1) = sigXps;
-    operator()(1,0) = sigXps;
+    operator()(0,0) = e_00;
+    operator()(0,1) = e_01;
+    operator()(0,2) = e_02;
+    operator()(0,3) = e_03;
+    
+    operator()(1,0) = e_10;
+    operator()(1,1) = e_11;
+    operator()(1,2) = e_12;
+    operator()(1,3) = e_13;
 
-    operator()(2,2) = sigYpos;
-    operator()(3,3) = sigYslp;
-    operator()(2,3) = sigYps;
-    operator()(3,2) = sigYps;
+    operator()(2,0) = e_20;
+    operator()(2,1) = e_21;
+    operator()(2,2) = e_22;
+    operator()(2,3) = e_23;
+
+    operator()(3,0) = e_30;
+    operator()(3,1) = e_31;
+    operator()(3,2) = e_32;
+    operator()(3,3) = e_33;
+
 }
+
 
 void TkrCovMat::initialize(const TkrCovMat& covMat)
 {
     ResizeTo(covMat);
     
     operator()(0,0) = covMat.getCovX0X0();
-    operator()(1,1) = covMat.getCovSxSx();
     operator()(0,1) = covMat.getCovX0Sx();
-    operator()(1,0) = covMat.getCovX0Sx();
-    
+    operator()(0,2) = covMat.getCovX0Y0();
+    operator()(0,3) = covMat.getCovX0Sy();
+
+    operator()(1,0) = covMat.getCovSxX0();
+    operator()(1,1) = covMat.getCovSxSx();
+    operator()(1,2) = covMat.getCovSxY0();
+    operator()(1,3) = covMat.getCovSxSy();
+
+    operator()(2,0) = covMat.getCovY0X0();
+    operator()(2,1) = covMat.getCovY0Sx();
     operator()(2,2) = covMat.getCovY0Y0();
-    operator()(3,3) = covMat.getCovSySy();
     operator()(2,3) = covMat.getCovY0Sy();
-    operator()(3,2) = covMat.getCovY0Sy();
+
+    operator()(3,0) = covMat.getCovSyX0();
+    operator()(3,1) = covMat.getCovSySx();
+    operator()(3,2) = covMat.getCovSyY0();
+    operator()(3,3) = covMat.getCovSySy();
 }
+
+
 
 void TkrCovMat::Print(Option_t *option) const {
     TObject::Print(option);
