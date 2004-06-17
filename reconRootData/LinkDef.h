eb9c622abaf818f15b65b9c30462477bf3a03d85
burnett <>
2004-06-17 04:53:24 +0000

    use RootPolicy pattern

---
+++ reconRootData/LinkDef.h
@@ -0,0 +1,30 @@
+/** @file LinkDef.h
+    @brief for rootcint
+ $Header$
+
+*/#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class ReconEvent+;
+
+#pragma link C++ class AcdRecon+;
+#pragma link C++ class CalRecon+;
+#pragma link C++ class CalCluster+;
+#pragma link C++ class CalRangeRecData+;
+#pragma link C++ class CalXtalRecData+;
+#pragma link C++ class TkrRecon+;
+#pragma link C++ class TkrCluster+;
+#pragma link C++ class TkrTrack+;
+#pragma link C++ class TkrKalFitTrack+;
+#pragma link C++ class TkrFitHit+;
+#pragma link C++ class TkrCandHit+;
+#pragma link C++ class TkrCandTrack+;
+#pragma link C++ class TkrVertex+;
+#pragma link C++ class TkrCovMat+;
+#pragma link C++ class TkrHitPlane+;
+#pragma link C++ class TkrParams+;
+
+#endif
