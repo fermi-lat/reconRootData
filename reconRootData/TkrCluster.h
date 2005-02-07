77eb4010788963afc637120a42eff630428acb85
lsrea <>
2005-02-07 20:35:44 +0000

    redo TkrCluster

--- reconRootData/TkrCluster.h
+++ reconRootData/TkrCluster.h
@@ -22,59 +22,53 @@
 */
 class TkrCluster : public TObject
 {
-	
+
 public:
-        // once we have an official release, the version number can be used
-        //  to allow backward compatibility
+    // once we have an official release, the version number can be used
+    //  to allow backward compatibility
 
-        enum {VERSION = 3};
+    enum {VERSION = 3};
 
-        // fields and shifts of the status word, which together make a mask
-        //  
+    // fields and shifts of the status word, which together make a mask
+    //  
 
-        enum { 
-            fieldUSED        = 1,    // tells whether cluster is used on a track
-            fieldEND         = 3,    // identifies controller, 0, 1, 2=mixed
-            fieldRAWTOT      = 0xff, // raw ToT
-            fieldPLANEOFFSET = 1,    // to calculate Plane number from Tray/Face (1 for LAT)
-            fieldLAYEROFFSET = 1,    // to calculate Layer number from Plane (0 for LAT)
-            fieldVERSION     = 0xf   // version of class, room for 15!
-        };
-        enum {    
-            shiftUSED        =  0, 
-            shiftEND         =  1,
-            shiftRAWTOT      =  8,
-            shiftPLANEOFFSET = 26,
-            shiftLAYEROFFSET = 27,
-            shiftVERSION     = 28 
-        };
-        enum {
-            maskUSED        = fieldUSED<<shiftUSED,
-            maskEND         = fieldEND<<shiftEND,
-            maskRAWTOT      = fieldRAWTOT<<shiftRAWTOT,
-            maskPLANEOFFSET = fieldPLANEOFFSET<<shiftPLANEOFFSET,
-            maskLAYEROFFSET = fieldLAYEROFFSET<<shiftLAYEROFFSET,
-            maskVERSION     = fieldVERSION<<shiftVERSION
-        };
-    
+    enum { 
+        fieldUSED        = 1,    // tells whether cluster is used on a track
+        fieldEND         = 3,    // identifies controller, 0, 1, 2=mixed
+        fieldPLANEOFFSET = 1,    // to calculate Plane number from Tray/Face (1 for LAT)
+        fieldLAYEROFFSET = 1,    // to calculate Layer number from Plane (0 for LAT)
+   };
+    enum {    
+        shiftUSED        =  0, 
+        shiftEND         =  1,
+        shiftPLANEOFFSET = 29,
+        shiftLAYEROFFSET = 30,
+    };
+    enum {
+        maskUSED        = fieldUSED<<shiftUSED,
+        maskEND         = fieldEND<<shiftEND,
+        maskPLANEOFFSET = fieldPLANEOFFSET<<shiftPLANEOFFSET,
+        maskLAYEROFFSET = fieldLAYEROFFSET<<shiftLAYEROFFSET,
+    };
+
     TkrCluster();
-	
+
     TkrCluster(commonRootData::TkrId tkrId, UInt_t istrip0, UInt_t istripf, 
-               const TVector3& position, Float_t ToT, UInt_t status, UInt_t nBad);
+        const TVector3& position, UInt_t rawToT, Float_t ToT, UInt_t status, UInt_t nBad);
 
     virtual ~TkrCluster();
-	
+
     /// resets all member variables to default values
     void Clear(Option_t *option="");
-	
+
     void Print(Option_t *option="") const;
-		
+
     inline commonRootData::TkrId getTkrId()      const { return m_tkrId;}
     inline UInt_t                getFirstStrip() const { return m_strip0; }
     inline UInt_t                getLastStrip()  const { return m_stripf; }
     inline Float_t               getToT()        const { return getRawToT(); }
     inline UInt_t                getNBad()       const { return m_nBad; }
-    
+
 
     /// Returns the center strip of this cluster
     inline Float_t getStrip() const { return (0.5*(m_strip0+m_stripf)); };
@@ -85,56 +79,46 @@ public:
     /// construct plane from tray/face
     inline UInt_t getStatusWord() const { return m_status; }
     inline int getPlane() const {
-        return m_tkrId.getTray()*2 + m_tkrId.getBotTop() - getPlaneOffset(); 
+        return m_tkrId.getTray()*2 + m_tkrId.getBotTop() - 1 + getPlaneOffset(); 
     }
     // construct layer from Plane
-    inline int getLayer()     const { return (getPlane() + getLayerOffset())/2; }
+    inline int getLayer() const { return (getPlane() + getLayerOffset())/2; }
 
     /// returns true if the cluster has been flagged
-    bool hitFlagged()     const { 
-        return ((maskVERSION&m_status)!=0 ? ((m_status&maskUSED)>0) : m_status!=0);}
+    bool hitFlagged()     const { return ((m_status&maskUSED)>0);}
 
     /// retrieves raw ToT (will be raw or corrected depending on the version)
-    inline Float_t getRawToT() const { 
-        return ( (m_status&maskVERSION)>2 ? (m_status&maskRAWTOT)>>shiftRAWTOT : m_ToT );
-    }
-    /// retrieve corrected ToT (zero for old-style records)
-    inline Float_t getMips() const {
-        return ( (m_status&maskVERSION)>2 ? m_ToT : 0.0 );
-    }
-    /// retrieve end
-    inline UInt_t getEnd() const {
-        return ( (m_status&maskVERSION)>2 ? (m_status&maskEND)>>shiftEND : 2 );
-    }
-    /// retrieve version number (old-style is zero)
-    inline UInt_t getVersion() const {
-        return m_status&maskVERSION>>shiftVERSION;
-    }
+    inline Float_t getRawToT() const { return m_rawToT; }
 
-	
+    /// retrieve corrected ToT (zero for old-style records)
+    inline Float_t getMips() const { return   m_ToT ; }
+    /// retrieve end
+    inline UInt_t getEnd() const { return ((m_status&maskEND)>>shiftEND);}
+
 private:
-        inline UInt_t getPlaneOffset() const { 
-            return ((maskVERSION & m_status)!=0 ? ((m_status&maskPLANEOFFSET)>>shiftPLANEOFFSET) : 1); }
-        inline UInt_t getLayerOffset() const { 
-            return ((maskVERSION & m_status)!=0 ? ((m_status&maskLAYEROFFSET)>>shiftLAYEROFFSET) : 0); }
-    
+    inline UInt_t getPlaneOffset() const { 
+        return ((m_status&maskPLANEOFFSET)>>shiftPLANEOFFSET); }
+    inline UInt_t getLayerOffset() const { 
+        return ((m_status&maskLAYEROFFSET)>>shiftLAYEROFFSET); }
+
     /// volume id
     commonRootData::TkrId m_tkrId;  
     /// initial strip address of the cluster
     UInt_t   m_strip0;
     /// final strip address of the cluster
     UInt_t   m_stripf;
-    /// space position of the cluster
-    TVector3 m_position;
-    /// ToT value of the cluster
-    Float_t m_ToT;    
-    /// Everything  below here is a candidate for removal
-    /// flag of the cluster, used during pattern recognition
-    UInt_t   m_status;
     /// number of bad strips in this cluster
     UInt_t   m_nBad;
+    /// space position of the cluster
+    TVector3 m_position;
+    /// Raw ToT
+    UInt_t m_rawToT;
+    /// ToT value of the cluster
+    Float_t m_ToT;    
+    /// flag of the cluster, used during pattern recognition
+    UInt_t   m_status;
 
-	ClassDef(TkrCluster,2) 
+    ClassDef(TkrCluster,2) 
 };
 
 #endif
