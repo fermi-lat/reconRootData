ec708145c246b0b06bd785bf6d97fe0bd9c548e1
chamont <>
2005-06-03 12:06:24 +0000

    add CalParams

--- reconRootData/LinkDef.h
+++ reconRootData/LinkDef.h
@@ -8,13 +8,18 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+#pragma link C++ class TVector3+;
+
 #pragma link C++ class ReconEvent+;
 
 #pragma link C++ class AcdRecon+;
+
 #pragma link C++ class CalRecon+;
+#pragma link C++ class CalParams+;
 #pragma link C++ class CalCluster+;
 #pragma link C++ class CalRangeRecData+;
 #pragma link C++ class CalXtalRecData+;
+
 #pragma link C++ class TkrRecon+;
 #pragma link C++ class TkrCluster+;
 #pragma link C++ class TkrTrack+;
---
+++ src/CalParams.cxx
@@ -0,0 +1,96 @@
+
+// File and Version information:
+// $Header$
+//
+//  Implementation file of Rot clone for CalParams
+//  
+// Authors:
+//
+//    Tracy Usher
+//    David Chamont
+//
+
+#include <reconRootData/CalParams.h>
+#include <iostream>
+
+ClassImp(CalParams)
+
+CalParams::CalParams
+( double energy, double eneError,
+  TVector3 centroid,
+  double cntdxx, double cntdxy, double cntdxz, double cntdyy, double cntdyz, double cntdzz,
+  TVector3 axis,
+  double axsdxx, double axsdxy, double axsdxz, double axsdyy, double axsdyz, double axsdzz )
+{
+    init
+    ( energy,eneError,
+      centroid,cntdxx,cntdxy,cntdxz,cntdyy,cntdyz,cntdzz,
+      axis,axsdxx,axsdxy,axsdxz,axsdyy,axsdyz,axsdzz ) ;
+}
+
+CalParams::CalParams( const CalParams & cp )
+{ operator=(cp) ; }
+
+CalParams & CalParams::operator=
+( const CalParams & cp )
+{
+    init
+    ( cp.getEnergy(),cp.getEnergyErr(),
+      cp.getCentroid(),
+      cp.getxPosxPos(),cp.getxPosyPos(),cp.getxPoszPos(),
+      cp.getyPosyPos(),cp.getyPoszPos(),cp.getzPoszPos(),
+      cp.getAxis(),
+      cp.getxDirxDir(),cp.getxDiryDir(),cp.getxDirzDir(),
+      cp.getyDiryDir(),cp.getyDirzDir(),cp.getzDirzDir() ) ;
+    return (*this) ;
+}
+
+void CalParams::Clear( Option_t * )
+{
+    init
+    ( 0.,0.,
+      TVector3(0.,0.,0.),0.,0.,0.,0.,0.,0.,
+      TVector3(0.,0.,0.),0.,0.,0.,0.,0.,0. ) ;
+}
+
+void CalParams::init
+( Double_t energy, Double_t eneError,
+  TVector3 centroid,
+  Double_t cntdxx, Double_t cntdxy, Double_t cntdxz,
+  Double_t cntdyy, Double_t cntdyz, Double_t cntdzz,
+  TVector3 axis,
+  Double_t axsdxx, Double_t axsdxy, Double_t axsdxz,
+  Double_t axsdyy, Double_t axsdyz, Double_t axsdzz )
+{
+    m_energy = energy ;
+    m_eneError = eneError ;
+    m_clusterCentroid = centroid ;
+    m_cenxx = cntdxx ;
+    m_cenxy = cntdxy ;
+    m_cenxz = cntdxz ;
+    m_cenyy = cntdyy ;
+    m_cenyz = cntdyz ;
+    m_cenzz = cntdzz ;
+    m_clusterAxis = axis ;
+    m_axisxx = axsdxx ;
+    m_axisxy = axsdxy ;
+    m_axisxz = axsdxz ;
+    m_axisyy = axsdyy ;
+    m_axisyz = axsdyz ;
+    m_axiszz = axsdzz ;
+}
+
+void CalParams::Print( Option_t * ) const
+{
+    std::cout
+      << m_energy << " " << m_eneError << "\n"
+      << m_clusterCentroid.X() << " " << m_clusterCentroid.Y() << " " << m_clusterCentroid.Z() << "\n"
+      << m_cenxx  << " " << m_cenxy << " " << m_cenxz << "\n"
+      << m_cenxy  << " " << m_cenyy << " " << m_cenyz << "\n"
+      << m_cenxz  << " " << m_cenyz << " " << m_cenzz << "\n"
+      << m_clusterAxis.X() << " " << m_clusterAxis.Y() << " " << m_clusterAxis.Z() << "\n"
+      << m_axisxx  << " " << m_axisxy << " " << m_axisxz << "\n"
+      << m_axisxy  << " " << m_axisyy << " " << m_axisyz << "\n"
+      << m_axisxz  << " " << m_axisyz << " " << m_axiszz ;
+}
+
