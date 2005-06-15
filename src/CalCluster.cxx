bc7a2e3e2d35758eb20ddacf35c71e6ac3536f2d
chamont <>
2005-06-15 13:41:12 +0000

    update for last CalCluster change

--- src/CalCluster.cxx
+++ src/CalCluster.cxx
@@ -4,9 +4,9 @@
 ClassImp(CalCluster)
 
 void CalCluster::init
- ( const std::vector<CalClusterLayerData> & layers,
-   const CalParams & params,
-   Double_t rmsLong, Double_t rmsTrans )
+ ( const std::vector<CalClusterLayerData> & layers, const CalParams & params,
+   Double_t rmsLong, Double_t rmsLongAsym, Double_t rmsTrans,
+   Int_t numTruncXtals, UInt_t statusBits )
  {
   std::vector<CalClusterLayerData>::const_iterator layer ;
   int i ;
@@ -14,7 +14,10 @@ void CalCluster::init
     m_layers[i] = (*layer) ;
   m_params = params ;
   m_rmsLong = rmsLong ;
+  m_rmsLongAsym = rmsLongAsym ;
   m_rmsTrans = rmsTrans ;
+  m_numTruncXtals = numTruncXtals ;
+  m_statusBits = statusBits ;
  }
 
 void CalCluster::Clear( Option_t * )
@@ -24,20 +27,24 @@ void CalCluster::Clear( Option_t * )
     m_layers[i].Clear() ;
   m_params.Clear() ;
   m_rmsLong = 0.0 ;
+  m_rmsLongAsym = 0.0 ;
   m_rmsTrans = 0.0 ;
+  m_numTruncXtals = 0 ;
+  m_statusBits = 0 ;
  }
 
 CalCluster::CalCluster
- ( const std::vector<CalClusterLayerData> & layers,
-   const CalParams & params,
-   Double_t rmsLong, Double_t rmsTrans )
- { init(layers,params,rmsLong,rmsTrans) ; }
+ ( const std::vector<CalClusterLayerData> & layers, const CalParams & params,
+   Double_t rmsLong, Double_t rmsLongAsym, Double_t rmsTrans,
+   Int_t numTruncXtals, UInt_t statusBits )
+ { init(layers,params,rmsLong,rmsLongAsym,rmsTrans,numTruncXtals,statusBits) ; }
 
 void CalCluster::Print( Option_t * ) const
  {
   m_params.Print() ;
-  using namespace std ;
-  cout<<"RMS Long: "<<m_rmsLong<<"  RMS Trans: "<<m_rmsTrans<<endl ;
-  cout<<"Number of layers: "<<ROOT_NUMCALLAYERS<<endl ;
+  std::cout
+    <<"No.Trunc Xtals "<<m_numTruncXtals<<"\n"
+    <<"RMS Long: "<<m_rmsLong<<"RMS Long Asym: "<<m_rmsLongAsym<<"  RMS Trans: "<<m_rmsTrans<<"\n"
+    <<"Number of layers: "<<ROOT_NUMCALLAYERS<<std::endl ;
  }
 
