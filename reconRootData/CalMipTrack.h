45abe0373f99d0e6d87af8cd48f9bcfd126f67c4
usher <>
2005-07-14 17:20:01 +0000

    Updating CalMipTrack class for Fred

--- reconRootData/CalMipTrack.h
+++ reconRootData/CalMipTrack.h
@@ -21,14 +21,16 @@ using namespace std;
 class CalMipTrack: public TObject
 {
 private:
-    TVector3          m_point;
-    TVector3          m_dir;
-    Int_t             m_ndofTrack;
-    Double_t          m_ki2Track;
-    Double_t          m_length;
-    Double_t          m_dTrack2C;
-    Double_t          m_dTrack2Edge;
-    Double_t          m_energy;
+    TVector3   m_point;
+    TVector3   m_dir;
+    Double_t   m_d2C;
+    Double_t   m_d2Edge;
+    Int_t      m_calEdge;
+    Double_t   m_arcLen;
+    Double_t   m_ecor;
+    Double_t   m_ecorRms;
+    Double_t   m_chi2;   
+    Double_t   m_erm;   
   
  public:
   
@@ -39,39 +41,48 @@ private:
   
     ~CalMipTrack()  {};
   
-    CalMipTrack(TVector3 point, 
-                TVector3 dir, 
-                Int_t    ndofTrack, 
-                Double_t ki2Track, 
-                Double_t length, 
-                Double_t dTrack2C, 
-                Double_t dTrack2Edge, 
-                Double_t energy) 
-                : m_point(point),
-                  m_dir(dir),
-                  m_ndofTrack(ndofTrack),
-                  m_ki2Track(ki2Track),
-                  m_length(length),
-                  m_dTrack2C(dTrack2C),
-                  m_dTrack2Edge(dTrack2Edge),
-                  m_energy(energy){}
-  
-    void      setPoint(TVector3 point)        {m_point       = point;      }
-    void      setDir(TVector3 vector)         {m_dir         = vector;     }
-    void      setNdof(Int_t ndof)             {m_ndofTrack   = ndof;       }
-    void      setKi2(Double_t ki2)            {m_ki2Track    = ki2;        }
-    void      setLength(Double_t length)      {m_length      = length;     }
-    void      setD2C(Double_t dTrack2C)       {m_dTrack2C    = dTrack2C;   }
-    void      setD2Edge(Double_t dTrack2Edge) {m_dTrack2Edge = dTrack2Edge;}
-    void      setEnergy(Double_t energy)      {m_energy      = energy;     }
-    const TVector3  getPoint() const          {return m_point;             }
-    const TVector3  getDir()   const          {return m_dir;               }
-    Int_t     getNdof() const                 {return m_ndofTrack;         }
-    Double_t  getKi2()  const                 {return m_ki2Track;          }
-    Double_t  getLength()  const              {return m_length;            }
-    Double_t  getD2C()     const              {return m_dTrack2C;          }
-    Double_t  getD2Edge()  const              {return m_dTrack2Edge;       }
-    Double_t  getEnergy()  const              {return m_energy;            }
+    CalMipTrack(TVector3 point,
+                TVector3 dir,
+                Double_t d2C,
+                Double_t d2Edge,
+                Int_t    calEdge,
+                Double_t arcLen,
+                Double_t ecor,
+                Double_t ecorRms,
+                Double_t chi2,
+                Double_t erm)
+      :m_point(point),
+       m_dir(dir),
+       m_d2C(d2C),
+       m_d2Edge(d2Edge),
+       m_calEdge(calEdge),
+       m_arcLen(arcLen),
+       m_ecor(ecor),
+       m_ecorRms(ecorRms),
+       m_chi2(chi2),
+       m_erm(erm){}
+    
+    void    setPoint   (TVector3 point)    {m_point    = point;    }
+    void    setDir     (TVector3 dir)      {m_dir      = dir;      }
+    void    setD2C     (Double_t d2C)      {m_d2C      = d2C;      }
+    void    setD2Edge  (Double_t d2Edge)   {m_d2Edge   = d2Edge;   }
+    void    setCalEdge (Int_t calEdge)     {m_calEdge  = calEdge;  }
+    void    setArcLen  (Double_t arcLen)   {m_arcLen   = arcLen;   }
+    void    setEcor    (Double_t ecor)     {m_ecor     = ecor;     }
+    void    setEcorRms (Double_t ecorRms)  {m_ecorRms  = ecorRms;  }
+    void    setChi2    (Double_t chi2)     {m_chi2     = chi2;     }
+    void    setErm     (Double_t erm)      {m_erm      = erm;      }
+
+    TVector3  getPoint   ()                {return m_point;        }
+    TVector3  getDir     ()                {return m_dir;          }
+    Double_t  getD2C     ()                {return m_d2C;          }
+    Double_t  getD2Edge  ()                {return m_d2Edge;       }
+    Int_t     getCalEdge ()                {return m_calEdge;      }
+    Double_t  getArcLen  ()                {return m_arcLen;       }
+    Double_t  getEcor    ()                {return m_ecor;         }
+    Double_t  getEcorRms ()                {return m_ecorRms;      }
+    Double_t  getChi2    ()                {return m_chi2;         }
+    Double_t  getErm     ()                {return m_erm;          }
   
     void print();
   
--- src/CalMipTrack.cxx
+++ src/CalMipTrack.cxx
@@ -6,21 +6,10 @@ ClassImp(CalMipTrack)
 //-----------------------------------------------------------------------------------------------------------------
 void CalMipTrack::print()
 {
-    std::cout<<"-----------------------------------------------------------------"<<std::endl;
-    std::cout<<" Print in CalMipTrack s Class"<<std::endl;
-
-    std::cout<< "length      =" << m_length       <<std::endl;
-    std::cout<< "dTrack2C    ="<< m_dTrack2C      <<std::endl;
-    std::cout<< "dTrack2Edge ="<< m_dTrack2Edge   <<std::endl;
-    std::cout<< "energy      ="<< m_energy        <<std::endl;
-    std::cout<< "Ndof        =" << m_ndofTrack    <<std::endl;
-    std::cout<< "Ki2         =" << m_ki2Track     <<std::endl;
-    std::cout<< "PosX        =" << getPoint().x() <<std::endl;
-    std::cout<< "PosY        =" << getPoint().y() <<std::endl;
-    std::cout<< "PosZ        =" << getPoint().z() <<std::endl;
-    std::cout<< "DirX        =" << getDir().x()   <<std::endl;
-    std::cout<< "DirY        =" << getDir().y()   <<std::endl;
-    std::cout<< "DirZ        =" << getDir().z()   <<std::endl;
-
-    int comptor=0;
+  std::cout << "---> writeOutPrint calMipTrack" << std::endl;
+  std::cout << "---> point =" << getPoint().x() << " " << getPoint().y() << " " << getPoint().z() << std::endl;
+  std::cout << "---> dir   =" << getDir().x()   << " " << getDir().y()   << " " << getDir().z()   << std::endl;
+  std::cout << "---> d2C / d2Edge / calEdge / arcLen = " << m_d2C << " " << m_d2Edge << " " << m_calEdge << " "<< m_arcLen << std::endl;
+  std::cout << "---> ecor / ecorRms / chi2 / erm = " << m_ecor << " " << m_ecorRms << " " << m_chi2 << " " << m_erm << std::endl;
+ 
 }
