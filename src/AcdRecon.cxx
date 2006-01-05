47fdee31a362008d3c69e2fbb1756e484d45587f
cvs2git <>
2006-01-05 22:42:53 +0000

    This commit was manufactured by cvs2svn to create tag 'eac_060105'.
    Sprout from master 2005-11-17 21:09:56 UTC heather 'AutoUpdate'
    Cherrypick from master 2006-01-05 22:42:52 UTC echarles 'added AcdHit and AcdTkrPoca to root library, got rid of spurious debugging printout':
        reconRootData/AcdHit.h
        reconRootData/AcdRecon.h
        reconRootData/AcdTkrIntersection.h
        reconRootData/AcdTkrPoca.h
        reconRootData/LinkDef.h
        src/AcdHit.cxx
        src/AcdRecon.cxx
        src/AcdTkrIntersection.cxx
        src/AcdTkrPoca.cxx
    Cherrypick from EM-v5r0703p5 2006-01-05 22:42:50 UTC echarles 'added AcdHit and AcdTkrPoca to root library, got rid of spurious debugging printout':
        cmt/requirements
        doc/release.notes
        reconRootData/TkrTrack.h
        reconRootData/TkrTrackHit.h
        reconRootData/TkrTrackParams.h
        reconRootData/TkrVertex.h

--- src/AcdRecon.cxx
+++ src/AcdRecon.cxx
@@ -1,12 +1,15 @@
 #include "reconRootData/AcdRecon.h"
+#include <commonRootData/RootDataUtil.h>
 #include <iostream>
 
 ClassImp(AcdRecon)
 
-static const maxDoca = 2000.0;
+static const Double_t maxDoca = 2000.0;
 
 AcdRecon::AcdRecon() {
   m_acdTkrIntersectionCol = 0;
+  m_acdTkrPocaCol = 0;
+  m_acdHitCol = 0;
   Clear();
 }
 
@@ -17,6 +20,17 @@ AcdRecon::~AcdRecon() {
   }
   delete m_acdTkrIntersectionCol;
   m_acdTkrIntersectionCol = 0;
+  if ( m_acdTkrPocaCol != 0 ) {
+    m_acdTkrPocaCol->Delete();
+  }
+  delete m_acdTkrPocaCol;
+  m_acdTkrPocaCol = 0;
+  if ( m_acdHitCol != 0 ) {
+    m_acdHitCol->Delete();
+  }
+  delete m_acdHitCol;
+  m_acdHitCol = 0;
+
 }
 
 void AcdRecon::initialize(Double_t e, Double_t ribbonE, Int_t count, 
@@ -81,7 +95,13 @@ void AcdRecon::Clear(Option_t* /*option*/) {
     if ( m_acdTkrIntersectionCol ) {
       m_acdTkrIntersectionCol->Clear();
     }
-    m_ribbonActDist = -maxDoca;
+    if ( m_acdTkrPocaCol ) {
+      m_acdTkrPocaCol->Clear();
+    }
+    if ( m_acdHitCol ) {
+      m_acdHitCol->Clear();
+    }
+     m_ribbonActDist = -maxDoca;
     m_cornerDoca = maxDoca;
 }
 
@@ -144,5 +164,109 @@ AcdTkrIntersection* AcdRecon::addAcdTkrIntersection(AcdTkrIntersection& inter) {
   TClonesArray& intersects = *m_acdTkrIntersectionCol;
   new (intersects[numInter]) AcdTkrIntersection(inter);  
   return (AcdTkrIntersection*)(intersects[numInter]);
+}
+
+
+AcdTkrPoca* AcdRecon::addAcdTkrPoca(AcdTkrPoca& poca) {
+  UInt_t numPoca = nAcdTkrPoca();
+  if ( m_acdTkrPocaCol == 0 ) {
+    m_acdTkrPocaCol = new TClonesArray( AcdTkrPoca::Class_Name() );
+  }
+  // fill the TClonesArray w/ a placement new call
+  TClonesArray& pocas = *m_acdTkrPocaCol;
+  new (pocas[numPoca]) AcdTkrPoca(poca);
+  return (AcdTkrPoca*)(pocas[numPoca]);
+}
+    
+
+AcdHit* AcdRecon::addAcdHit(AcdHit& hit) {
+  UInt_t numHit = nAcdHit();
+  if ( m_acdHitCol == 0 ) {
+    m_acdHitCol = new TClonesArray( AcdHit::Class_Name() );
+  }
+  // fill the TClonesArray w/ a placement new call
+  TClonesArray& hits = *m_acdHitCol;
+  new (hits[numHit]) AcdHit(hit);
+  return (AcdHit*)(hits[numHit]);
+}
+    
+
+//======================================================
+// For Unit Tests
+//======================================================
+
+void AcdRecon::Fake( Int_t ievent, Float_t randNum ) {
+
+    Float_t f = Float_t(ievent);
+    Double_t energy = f ;
+    Double_t ribbonE = energy;
+    Int_t count = 5;
+    Int_t ribbonCount = 2;
+    Double_t gDoca = f*randNum;
+    Double_t doca = randNum;
+    Double_t actDist = 2.*f;
+    Double_t ribActDist = 3.*f;
+    AcdId minDocaId(0, 0, 3, 2);
+    AcdId maxActDistId(0,0,2,2);
+    AcdId ribActDistId(0,1,4,3);
+    ribActDistId.setReadoutIndex(ievent) ;
+    std::vector<Double_t> rowDocaCol;
+    rowDocaCol.push_back(randNum);
+    rowDocaCol.push_back(f);
+    std::vector<Double_t> rowActDistCol ;
+    rowActDistCol.push_back(randNum) ;
+    rowActDistCol.push_back(f) ;
+    std::vector<AcdId> idCol ;
+    idCol.push_back(AcdId(0, 0, 3, 2)) ;
+    idCol.push_back(AcdId(0, 2, 2, 1)) ;
+    std::vector<Double_t> energyCol ;
+    energyCol.push_back(f) ;
+    energyCol.push_back(2.*f) ;
+    Double32_t cornerDoca = f*randNum ;
+    
+    initialize(
+        energy, ribbonE, count, ribbonCount, gDoca, doca, 
+        minDocaId, actDist, maxActDistId,
+        ribActDist, ribActDistId,
+        rowDocaCol, rowActDistCol,
+        idCol, energyCol, cornerDoca
+    ) ;
+ }
+
+#define UTIL_COMPARE_IN_RANGE(att) rootdatautil::CompareInRange(get ## att(),a.get ## att(),#att)
+#define ID_COMPARE_IN_RANGE(att) get ## att().CompareInRange(a.get ## att(),#att)
+
+Bool_t AcdRecon::CompareInRange( const AcdRecon & a, const std::string & name ) const {
+
+    bool result = true ;
+
+    result = UTIL_COMPARE_IN_RANGE(Energy) && result ;
+    result = UTIL_COMPARE_IN_RANGE(RibbonEnergy) && result ;
+    result = UTIL_COMPARE_IN_RANGE(TileCount) && result ;
+    result = UTIL_COMPARE_IN_RANGE(RibbonCount) && result ;
+    result = UTIL_COMPARE_IN_RANGE(GammaDoca) && result ;
+    result = UTIL_COMPARE_IN_RANGE(Doca) && result ;
+    result = ID_COMPARE_IN_RANGE(MinDocaId) && result ;
+    result = UTIL_COMPARE_IN_RANGE(ActiveDist) && result ;
+    result = UTIL_COMPARE_IN_RANGE(RibbonActiveDist) && result ;
+    result = ID_COMPARE_IN_RANGE(MinDocaId) && result ;
+    result = ID_COMPARE_IN_RANGE(MaxActDistId) && result ;
+    result = ID_COMPARE_IN_RANGE(RibbonActDistId) && result ;
+    result = UTIL_COMPARE_IN_RANGE(RowDocaCol) && result ;
+    result = UTIL_COMPARE_IN_RANGE(RowActDistCol) && result ;
+    result = UTIL_COMPARE_IN_RANGE(EnergyCol) && result ;
+    result = UTIL_COMPARE_IN_RANGE(IdCol) && result ;
+
+    if (!result) {
+        if ( name == "" ) {
+            std::cout<<"Comparison ERROR for "<<ClassName()<<std::endl ;
+        }
+        else {
+            std::cout<<"Comparison ERROR for "<<name<<std::endl ;
+        }
+    }
+
+    return result ;
+
 }
 
