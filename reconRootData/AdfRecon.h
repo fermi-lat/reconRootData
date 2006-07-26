1ee04336c8bc43bcc38783523ec01e1051b65b63
heather <>
2006-07-26 22:03:55 +0000

    updates for final adf recon

--- reconRootData/AdfRecon.h
+++ reconRootData/AdfRecon.h
@@ -8,6 +8,7 @@
 
 #include "TaggerCluster.h"
 #include "commonRootData/adf/QdcHit.h"
+#include "commonRootData/adf/ScalerHit.h"
 
 /** @class AdfRecon
  * @brief The digitization ancillary data for beamtest 2006  
@@ -28,11 +29,55 @@ public:
 
     void Print(Option_t *option="") const;
 
-    void setEventNumber(UInt_t evtNum) { m_eventNumber = evtNum;}
-    void setSpillNumber(UInt_t spillNum) { m_spillNumber = spillNum; }
+    void initEventNumber(UInt_t evtNum) { m_eventNumber = evtNum;}
+    void initSpillNumber(UInt_t spillNum) { m_spillNumber = spillNum; }
+    void initNmodule(UInt_t n) { m_nModule = n; }
 
     UInt_t getEventNumber() const { return m_eventNumber; }
     UInt_t getSpillNumber() const { return m_spillNumber; }
+    UInt_t getNmodule() const { return m_nModule; }
+
+    void initNumHighestCluster(UInt_t n) {m_numHighestCluster = n;}
+    UInt_t getNumHighestCluster() const { return m_numHighestCluster; }
+
+    void initXyz(const Double_t *x, const Double_t *y, const Double_t *z, UInt_t n);
+
+    Double_t getX(Int_t module) const { 
+        if ((module >= 0) && (module < m_nModule))
+            return m_x[module]; 
+        else return 0.0; }
+    Double_t getY(Int_t module) const { 
+        if((module >= 0) && (module < m_nModule))
+           return m_y[module];
+        else return 0.0;  }
+    Double_t getZ(Int_t module) const { 
+        if((module >= 0) && (module < m_nModule))
+            return m_z[module]; 
+        else return 0.0; }
+
+    void init(Double_t px, Double_t py, Double_t pz) {
+        m_px = px; m_py = py; m_pz = pz; }
+
+    Double_t getPx() const { return m_px; }
+    Double_t getPy() const { return m_py; }
+    Double_t getPz() const { return m_pz; }
+
+    void initPhi(Double_t in, Double_t out) {
+        m_phiIn = in; m_phiOut = out; }
+
+    Double_t getPhiIn() const { return m_phiIn; }
+    Double_t getPhiOut() const { return m_phiOut; }
+
+    void initThetaPhi(Double_t t, Double_t p) {
+        m_theta = t; m_dphi = p; }
+
+    Double_t getTheta() const { return m_theta; }
+    Double_t getDeltaPhi() const { return m_dphi; }
+
+    void initEnergy(Double_t eRec, Double_t eCorr) {
+        m_eRec = eRec; m_eCorr = eCorr; }
+    Double_t getReconstructedEnergy() const { return m_eRec; }
+    Double_t getCorrectedEnergy() const { return m_eCorr; }
 
     const TObjArray* getTaggerClusterCol() const { return m_taggerClusterCol; }
     const TaggerCluster* getTaggerCluster(UInt_t ind) const;
@@ -41,8 +86,11 @@ public:
 
     const TClonesArray* getQdcHitCol() const { return m_qdcHitCol; }
     const commonRootData::QdcHit* getQdcHit(UInt_t ind) const;
-    commonRootData::QdcHit* addQdcHit(UInt_t channel, UInt_t pulseHgt, UInt_t module, Bool_t isPedSub);
+    commonRootData::QdcHit* addQdcHit(UInt_t channel, Double_t pulseHgt, UInt_t module, Double_t sigma, Bool_t isPedSub);
 
+    const TClonesArray* getScalerHitCol() const { return m_scalerHitCol; }
+    const commonRootData::ScalerHit* getScalerHit(UInt_t ind) const;
+    commonRootData::ScalerHit* addScalerHit(UInt_t chan, UInt_t val);
 
     void Fake( Int_t ievent, Float_t randNum );
     Bool_t CompareInRange( const AdfRecon &, const std::string & name = "" )const;
@@ -52,11 +100,29 @@ private:
     UInt_t m_eventNumber;
     UInt_t m_spillNumber;
 
+    UInt_t m_numHighestCluster;
     Int_t m_numQdcHit;
+    Int_t m_numScalerHit;
+
+    Int_t m_nModule;
+    Double32_t *m_x; //[m_nModule]
+    Double32_t *m_y; //[m_nModule]
+    Double32_t *m_z; //[m_nModule]
+
+    Double32_t m_px;
+    Double32_t m_py;
+    Double32_t m_pz;
+    Double32_t m_eRec;
+    Double32_t m_eCorr;
+    Double32_t m_phiIn;
+    Double32_t m_phiOut;
+    Double32_t m_theta;
+    Double32_t m_dphi;
 
     TObjArray *m_taggerClusterCol;
 
     TClonesArray *m_qdcHitCol;
+    TClonesArray *m_scalerHitCol;
 
     ClassDef(AdfRecon,1) // ReconAncillary data beamtest 2006
 };
--- src/AdfRecon.cxx
+++ src/AdfRecon.cxx
@@ -14,9 +14,20 @@ AdfRecon::AdfRecon() {
     m_taggerClusterCol = 0; 
     m_qdcHitCol = 0; 
     m_numQdcHit = -1;
+    m_scalerHitCol = 0;
+    m_numScalerHit = -1;
 
     m_eventNumber = 0;
     m_spillNumber = 0;
+    m_px = 0.0; m_py = 0.0; m_pz = 0.0;
+    m_eRec = 0.0; m_eCorr = 0.0;
+    m_phiIn = 0.0; m_phiOut = 0.0;
+    m_theta = 0.0; m_dphi = 0.0;
+    m_numHighestCluster  = 0;
+    m_nModule = 0;
+    m_x = 0;
+    m_y = 0;
+    m_z = 0;
 }
 
 
@@ -34,6 +45,14 @@ AdfRecon::~AdfRecon() {
         m_qdcHitCol = 0;
     }
     
+    if (m_scalerHitCol) {
+        m_scalerHitCol->Delete();
+        delete m_scalerHitCol;
+        m_scalerHitCol = 0;
+    }
+    /*if (m_x) { delete [] m_x; m_x = 0; }
+    if (m_y) { delete [] m_y; m_y = 0; }
+    if (m_z) { delete [] m_z; m_z = 0; }*/
 }
 
 
@@ -46,6 +65,19 @@ void AdfRecon::Clear(Option_t *option) {
     if (m_taggerClusterCol) m_taggerClusterCol->Clear("C");
     if (m_qdcHitCol) m_qdcHitCol->Clear("C");
     m_numQdcHit = -1;
+    if (m_scalerHitCol) m_scalerHitCol->Clear("C");
+    m_numScalerHit = -1;
+
+    if (m_x) { delete [] m_x; m_x = 0; }
+    if (m_y) { delete [] m_y; m_y = 0; }
+    if (m_z) { delete [] m_z; m_z = 0;} 
+
+    m_px = 0.0; m_py = 0.0; m_pz = 0.0;
+    m_eRec = 0.0; m_eCorr = 0.0;
+    m_phiIn = 0.0; m_phiOut = 0.0;
+    m_theta = 0.0; m_dphi = 0.0;
+    m_numHighestCluster  = 0;
+    m_nModule = 0;
 
 }
 
@@ -57,9 +89,24 @@ void AdfRecon::Print(Option_t *option) const {
     cout << "EventNumber: " << m_eventNumber << " SpillNumber: " 
          << m_spillNumber << endl;
     cout << "Num Tagger Clusters: " << m_taggerClusterCol->GetEntriesFast() 
-         << " Num Qdc Hits: " << m_numQdcHit << endl;
+         << "Num Scaler Hits: " << m_numScalerHit+1
+         << " Num Qdc Hits: " << m_numQdcHit+1 << endl;
     cout << dec;
 }
+
+void AdfRecon::initXyz(const Double_t *x, const Double_t *y, const Double_t *z, UInt_t n) {
+    m_nModule = n;
+    m_x = new Double32_t[n];
+    m_y = new Double32_t[n];
+    m_z = new Double32_t[n];
+    UInt_t i;
+    for (i=0; i<n; i++) {
+        m_x[i] = x[i];
+        m_y[i] = y[i];
+        m_z[i] = z[i];
+    }
+}
+
 
 void  AdfRecon::addTaggerCluster(TaggerCluster* cluster) { 
     if (!m_taggerClusterCol) m_taggerClusterCol = new TObjArray();
@@ -73,14 +120,14 @@ const reconRootData::TaggerCluster* AdfRecon::getTaggerCluster(UInt_t ind) const
         return 0;
 }
 
-commonRootData::QdcHit* AdfRecon::addQdcHit(UInt_t channel, UInt_t pulseHgt, UInt_t module, Bool_t isPedSub) {
+commonRootData::QdcHit* AdfRecon::addQdcHit(UInt_t channel, Double_t pulseHgt, UInt_t module, Double_t sigma, Bool_t isPedSub) {
     // Add a new QdcHit entry, note that
     // TClonesArrays can only be filled via
     // a new with placement call
     if (!m_qdcHitCol) m_qdcHitCol = new TClonesArray("commonRootData::QdcHit",1);
     ++m_numQdcHit;
     TClonesArray &localCol = *m_qdcHitCol;
-    new(localCol[m_numQdcHit]) commonRootData::QdcHit(channel, pulseHgt, module, isPedSub);
+    new(localCol[m_numQdcHit]) commonRootData::QdcHit(channel, pulseHgt, module, sigma, isPedSub);
     return ((commonRootData::QdcHit*)(localCol[m_numQdcHit]));
 }
 
@@ -91,9 +138,26 @@ const commonRootData::QdcHit* AdfRecon::getQdcHit(UInt_t ind) const {
         return 0;
 }
 
+commonRootData::ScalerHit* AdfRecon::addScalerHit(UInt_t channel, UInt_t val) {
+    // Add a new ScalerHit entry, note that
+    // TClonesArrays can only be filled via
+    // a new with placement call
+    if (!m_scalerHitCol) m_scalerHitCol = new TClonesArray("commonRootData::ScalerHit",1);
+    ++m_numScalerHit;
+    TClonesArray &localCol = *m_scalerHitCol;
+    new(localCol[m_numScalerHit]) commonRootData::ScalerHit(channel, val);
+    return ((commonRootData::ScalerHit*)(localCol[m_numScalerHit]));
+}
+
+const commonRootData::ScalerHit* AdfRecon::getScalerHit(UInt_t ind) const {
+    if (((Int_t)ind) < m_scalerHitCol->GetEntriesFast()) 
+        return ((commonRootData::ScalerHit*)m_scalerHitCol->At(ind));
+    else
+        return 0;
+}
 void AdfRecon::Fake( Int_t ievent, Float_t randNum ) {
-    setEventNumber(ievent);
-    setSpillNumber(5);
+    initEventNumber(ievent);
+    initSpillNumber(5);
     // Add 2 TaggerClusters
     reconRootData::TaggerCluster* clus1 = new TaggerCluster();
     reconRootData::TaggerCluster* clus2 = new TaggerCluster();
@@ -105,9 +169,24 @@ void AdfRecon::Fake( Int_t ievent, Float_t randNum ) {
     addTaggerCluster(clus2);
    
     // Add 3 QdcHits
-    addQdcHit(1,2,3,true);
-    addQdcHit(2,3,4,false);
-    addQdcHit(3,4,5,true);
+    addQdcHit(1,2.2,3,4.4,true);
+    addQdcHit(2,3.3,4,5.5,false);
+    addQdcHit(3,4.4,5,6.6,true);
+
+    addScalerHit(6,7);
+
+    initNumHighestCluster(8);
+    init(1.1, 2.2, 3.3);
+
+    initPhi(4.4, 5.5);
+    initThetaPhi(6.6, 7.7);
+    initEnergy(8.8, 9.9);
+
+    Double_t x[3]={1.2, 2.3, 4.3};
+    Double_t y[3]={1.2, 2.3, 4.3};
+    Double_t z[3]={1.2, 2.3, 4.3};
+    initXyz(x,y,z,3);
+
 }
 
 Bool_t AdfRecon::CompareInRange( const AdfRecon &ref, const std::string& name ) const {
@@ -115,9 +194,20 @@ Bool_t AdfRecon::CompareInRange( const AdfRecon &ref, const std::string& name ) 
 
     result = rootdatautil::CompareInRange(getEventNumber(),ref.getEventNumber(),"EventNumber") && result;
     result = rootdatautil::CompareInRange(getSpillNumber(),ref.getSpillNumber(),"SpillNumber") && result;
+    result = rootdatautil::CompareInRange(getNumHighestCluster(),ref.getNumHighestCluster(),"numHighestsCluster") && result;
+    result = rootdatautil::CompareInRange(getPx(),ref.getPx(),"Px") && result;
+    result = rootdatautil::CompareInRange(getPy(),ref.getPy(),"Py") && result;
+    result = rootdatautil::CompareInRange(getPz(),ref.getPz(),"Pz") && result;
+    result = rootdatautil::CompareInRange(getPhiIn(),ref.getPhiIn(),"PhiIn") && result;
+    result = rootdatautil::CompareInRange(getPhiOut(),ref.getPhiOut(),"PhiOut") && result;
+    result = rootdatautil::CompareInRange(getTheta(),ref.getTheta(),"Theta") && result;
+    result = rootdatautil::CompareInRange(getDeltaPhi(),ref.getDeltaPhi(),"DeltaPhi") && result;
+    result = rootdatautil::CompareInRange(getReconstructedEnergy(),ref.getReconstructedEnergy(),"ReconstructedEnergy") && result;
+    result = rootdatautil::CompareInRange(getCorrectedEnergy(),ref.getCorrectedEnergy(),"CorrectedEnergy") && result;
 
     result = rootdatautil::TObjArrayCompareInRange<reconRootData::TaggerCluster>(m_taggerClusterCol,ref.m_taggerClusterCol) && result;
     result = rootdatautil::TObjArrayCompareInRange<commonRootData::QdcHit>(m_qdcHitCol,ref.m_qdcHitCol) && result;
+    result = rootdatautil::TObjArrayCompareInRange<commonRootData::ScalerHit>(m_scalerHitCol,ref.m_scalerHitCol) && result;
 
     if (!result) {
         if (name == "") {
