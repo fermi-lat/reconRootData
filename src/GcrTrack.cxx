5d507fe748358ef5f34c0a6ca247538cf608ac52
heather <>
2006-11-27 04:52:03 +0000

    remove cint files

---
+++ src/GcrTrack.cxx
@@ -0,0 +1,61 @@
+
+#include <reconRootData/GcrTrack.h>
+#include <commonRootData/RootDataUtil.h>
+#include "Riostream.h"
+
+ClassImp(GcrTrack)
+
+//-----------------------------------------------------------------------------------------------------------------
+void GcrTrack::Print(Option_t *option) const
+{
+  std::cout << "---> writeOutPrint GcrTrack" << std::endl;
+ 
+}
+
+
+//===================================================
+// dummy data, just for tests
+//===================================================
+
+/**void GcrTrack::Fake( Int_t , UInt_t  , Float_t ) 
+   {setPoint(TVector3(1.0,1.5, 1.9)) ;
+    setDir(TVector3(2.0, 2.5, 2.9)) ;
+    setD2C(1.) ;
+    setD2Edge(2.) ;
+    setCalEdge(3) ;
+    setArcLen(4.) ;
+    setEcor(5.) ;
+    setEcorRms(6.) ;
+    setChi2(7.) ;
+    setErm(8.) ;
+ }*/
+
+/**#define COMPARE_IN_RANGE(att) rootdatautil::CompareInRange(get ## att(),ref.get ## att(),#att)
+
+Bool_t GcrTrack::CompareInRange( const GcrTrack & ref, const std::string & name ) const {
+
+    bool result = true ;
+
+    result = COMPARE_IN_RANGE(Point) && result ;
+    result = COMPARE_IN_RANGE(Dir) && result ;
+    result = COMPARE_IN_RANGE(D2C) && result ;
+    result = COMPARE_IN_RANGE(D2Edge) && result ;
+    result = COMPARE_IN_RANGE(CalEdge) && result ;
+    result = COMPARE_IN_RANGE(ArcLen) && result ;
+    result = COMPARE_IN_RANGE(Ecor) && result ;
+    result = COMPARE_IN_RANGE(EcorRms) && result ;
+    result = COMPARE_IN_RANGE(Chi2) && result ;
+    result = COMPARE_IN_RANGE(Erm) && result ;
+
+    if (!result) {
+        if ( name == "" ) {
+            std::cout<<"Comparison ERROR for "<<ClassName()<<std::endl ;
+        }
+        else {
+            std::cout<<"Comparison ERROR for "<<name<<std::endl ;
+        }
+    }
+    return result ;
+
+}*/
+
