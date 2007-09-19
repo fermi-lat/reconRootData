6d4aed9dfc1a8d2858d97bf28e2b5f1d44b9014e
heather <>
2007-09-19 03:50:36 +0000

    avoid use of intVec and floatVec typedefs for now

--- reconRootData/TkrTruncationData.h
+++ reconRootData/TkrTruncationData.h
@@ -11,8 +11,9 @@ using std::vector;
 using namespace std;
 #endif
 #endif
-typedef std::vector<UInt_t>   intVector;
-typedef std::vector<Float_t> floatVector;
+// rather not pollute the namespace with these typedefs
+//typedef std::vector<UInt_t>   intVector;
+//typedef std::vector<Float_t> floatVector;
 
 /** @class TkrTruncationData
  * This ROOT class is the flattened version of the TDS 
@@ -27,19 +28,19 @@ class TkrTruncationData : public TObject
   virtual ~TkrTruncationData() {Clear();}
 
   const Int_t getStatus() const { return m_status; }
-  const intVector&   getStripCount() const   { return m_stripCount; }
-  const intVector&   getStripNumber() const { return m_stripNumber; }
-  const floatVector& getLocalX() const      { return m_localX; }
-  const float        getPlaneZ() const      { return m_planeZ; }
-  const int getTower() const { return m_tower;}
-  const int getTray()  const { return m_tray;}
-  const int getFace()  const { return m_face;}
-  const int getView()  const { return m_view; }
+  const vector<UInt_t>&   getStripCount() const   { return m_stripCount; }
+  const vector<UInt_t>&   getStripNumber() const { return m_stripNumber; }
+  const vector<Float_t>& getLocalX() const      { return m_localX; }
+  const Float_t        getPlaneZ() const      { return m_planeZ; }
+  const Int_t getTower() const { return m_tower;}
+  const Int_t getTray()  const { return m_tray;}
+  const Int_t getFace()  const { return m_face;}
+  const Int_t getView()  const { return m_view; }
 
   void setStatus(int status) { m_status = status; }
-  void setStripCount(const intVector& stripCount)  { m_stripCount = stripCount; }
-  void setStripNumber(const intVector& stripNumber) { m_stripNumber = stripNumber; }
-  void setLocalX(const floatVector& localX)      { m_localX = localX; }
+  void setStripCount(const vector<UInt_t>& stripCount)  { m_stripCount = stripCount; }
+  void setStripNumber(const vector<UInt_t>& stripNumber) { m_stripNumber = stripNumber; }
+  void setLocalX(const vector<Float_t>& localX)      { m_localX = localX; }
   void setPlaneZ(float planeZ)      { m_planeZ = planeZ; }
   void setTower(int tower)  { m_tower = tower;}
   void setTray(int tray)  { m_tray = tray;}
@@ -53,16 +54,16 @@ class TkrTruncationData : public TObject
 
  private:
   ///From TkrTruncationPlane
-  int m_status;
-  intVector m_stripCount;
-  intVector m_stripNumber;
-  floatVector m_localX;
-  float m_planeZ;
+  Int_t m_status;
   ///From TkrTruncationInfo
-  int m_tower;
-  int m_tray;
-  int m_face;
-  int m_view;
+  Int_t m_tower;
+  Int_t m_tray;
+  Int_t m_face;
+  Int_t m_view;
+  Float_t m_planeZ;
+  vector<UInt_t> m_stripCount;
+  vector<UInt_t> m_stripNumber;
+  vector<Float_t> m_localX;
 
   ClassDef(TkrTruncationData,1) 
 };
