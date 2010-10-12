2c7353df6824081db33f67c8d881579f342dad59
bregeon <>
2010-10-12 16:09:16 +0000

    new CalMSTreeParams library to be created

--- reconRootData/LinkDef.h
+++ reconRootData/LinkDef.h
@@ -36,6 +36,7 @@
 #pragma link C++ class CalRecon+;
 #pragma link C++ class CalParams+;
 #pragma link C++ class CalFitParams+;
+#pragma link C++ class CalMSTreeParams+;
 #pragma link C++ class CalClusterLayerData+;
 #pragma link C++ class CalCluster+;
 #pragma link C++ class CalCorToolResult+;
