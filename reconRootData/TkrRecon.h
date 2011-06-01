8f55427f2620a482202f9591d255dcada978c43f
kadrlica <>
2011-06-01 10:24:12 +0000

    Added separate cosmic-ray track collection

--- reconRootData/TkrRecon.h
+++ reconRootData/TkrRecon.h
@@ -45,6 +45,9 @@ public:
 
     TObjArray *getTrackCol() { return m_trackCol; };
     void addTrack(TObject* track) { m_trackCol->Add(track); };
+    /// ADW: Added cosmic-ray track collection (eventually make a map of collections?)
+    TObjArray *getCRTrackCol() { return m_crTrackCol; };
+    void addCRTrack(TObject* track) { m_crTrackCol->Add(track); };
 
     TObjArray *getVertexCol() { return m_vertexCol; };
     void addVertex(TkrVertex* vertex) { m_vertexCol->Add(vertex); };
@@ -55,11 +58,13 @@ public:
     // Added for macros & interactive access - EAC
     Int_t nCluster() const { return m_clusterCol->GetEntries() ; }    
     Int_t nTrack() const { return m_trackCol->GetEntries() ; }
+    Int_t nCRTrack() const { return m_crTrackCol->GetEntries() ; }
     Int_t nVertex() const { return m_vertexCol->GetEntries() ; }
     Int_t nTruncationData() const { return m_truncationDataCol->GetEntries() ; }
 
     TkrCluster* getCluster(Int_t i) { return i < nCluster() ? dynamic_cast<TkrCluster*>(m_clusterCol->At(i)) : 0; };
     TkrTrack* getTrack(Int_t i) { return i < nTrack() ? dynamic_cast<TkrTrack*>(m_trackCol->At(i)) : 0; };
+    TkrTrack* getCRTrack(Int_t i) { return i < nCRTrack() ? dynamic_cast<TkrTrack*>(m_crTrackCol->At(i)) : 0; };
     TkrVertex* getVertex(Int_t i) { return i < nVertex() ? dynamic_cast<TkrVertex*>(m_vertexCol->At(i)) : 0; };
     TkrTruncationData* getTruncationData(Int_t i) { return i < nTruncationData() ? dynamic_cast<TkrTruncationData*>(m_truncationDataCol->At(i)) : 0; };
     
@@ -72,6 +77,8 @@ private:
     TObjArray *m_clusterCol;
     /// collection of TkrTrack
     TObjArray *m_trackCol;
+    /// ADW: cosmic-ray collection of TkrTrack
+    TObjArray *m_crTrackCol;
     /// collection of TkrVertex
     TObjArray *m_vertexCol;
      /// Diagnostics
