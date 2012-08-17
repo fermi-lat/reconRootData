d28cf3657c68563e0f9dee7fcfdc440cbe91c4c6
jrb <>
2012-08-17 18:39:53 +0000

    patches for Windows no-install-headers

--- reconRootDataLib.py
+++ reconRootDataLib.py
@@ -2,7 +2,11 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['reconRootData'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'reconRootData') 
     env.Tool('commonRootDataLib')
     env.Tool('addLibrary', library = env['rootLibs'])
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'enums') 
 def exists(env):
     return 1;
